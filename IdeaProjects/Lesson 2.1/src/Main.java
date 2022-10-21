6968720f4f4210caa12c32fb49c9177fac161590
Karakaten <obryvkov1998@gmail.com>
2022-10-21 17:55:30 +0400

    тыкк

--- IdeaProjects/Lesson 2.1/src/Main.java
+++ IdeaProjects/Lesson 2.1/src/Main.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Journal journal = new Journal();
+        Student student = new Student();
 
 
         while (true) {
             System.out.println("Введите имя ученика");
-            journal.setFirstName(scanner.next());
+            student.setFirstName(scanner.next());
             System.out.println("Введите фамилию ученика");
-            journal.setSecondName(scanner.next());
+            student.setSecondName(scanner.next());
             System.out.println("Введите оценку за четверть");
-            journal.setGrade(scanner.nextInt());
+            student.setGrade(scanner.nextInt());
             System.out.println("Нажмите Завершить, чтобы выйти из программы. Чтобы продолжить – введите любой символ");
             scanner.nextLine();
             if (scanner.nextLine().equals("Завершить")) {
@@ -20,6 +20,6 @@ public class Main {
             }
         }
 
-        System.out.println(journal.out());
+        System.out.println(student.out());
     }
 }
